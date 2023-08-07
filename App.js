8ba3515dd5a4d5e03564b23c8c9f985db121f2f4
Manuel <manuel.gon.pos@gmail.com>
2023-08-07 18:16:42 +0200

    Estructura directorios mejor.

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import {SafeAreaView, } from 'react-native-safe-area-context';
 import { StyleSheet, View } from 'react-native';
-import { PassInput } from './PassInput.js';
-import { UserNameInput } from './UserNameInput.js';
+import { PassInput } from './src/components/PassInput.js';
+import { UserNameInput } from './src/components/UserNameInput.js';
 
 //http://localhost:3001/
 export default function App() {
   return (
-    <SafeAreaView style={baseView.container}>
-      <View style={{ flex: 0.5, backgroundColor: '#D2B48C' }}></View>
-      <View style={{ flex: 10 }}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.chHead}></View>
+      <View style={styles.chMain}>
         <UserNameInput />
         <PassInput />
       </View>
-      <View style={{ flex: 0.5, backgroundColor: '#BFC9A2' }}></View>
+      <View style={styles.chFoot}></View>
     </SafeAreaView>
   );
 }
 
-const baseView = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#FFFDD0',
   },
+  chMain: {
+     flex: 10 
+  },
+  chHead: {
+    flex: 0.5,
+    backgroundColor: '#D2B48C'
+  },
+  chFoot: {
+    flex: 0.5,
+    backgroundColor: '#BFC9A2'
+  }
+
 });
 
 
